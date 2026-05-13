fc57bff3f91bb7f6867825de5e7fa1f51ee3ff64
muhammadisa-n <muhammadisa226@gmail.com>
2026-05-14 00:19:36 +0700

    remove middleware appkey on routes public

--- src/routes/public-route.ts
+++ src/routes/public-route.ts
@@ -5,38 +5,12 @@ import { MessageController } from "../controllers/message-controller";
 import { ToolController } from "../controllers/tool-controller";
 import { FileController } from "../controllers/file-controller";
 import { TranslationController } from "../controllers/translation-controller";
-import { apiKeyMiddleware } from "../middleware/api-key-middleware";
-import { asyncHandler } from "../utils/async-handler";
 export const publicRouter = express.Router();
 
 // Example routes:
-publicRouter.get(
-  "/api/public/projects",
-  asyncHandler(apiKeyMiddleware),
-  ProjectController.getAll
-);
-publicRouter.get(
-  "/api/public/projects/count",
-  asyncHandler(apiKeyMiddleware),
-  ProjectController.count
-);
-publicRouter.get(
-  "/api/public/tools/",
-  asyncHandler(apiKeyMiddleware),
-  ToolController.getAll
-);
-publicRouter.post(
-  "/api/public/messages/",
-  asyncHandler(apiKeyMiddleware),
-  MessageController.create
-);
-publicRouter.get(
-  "/api/public/files/download/:id",
-  asyncHandler(apiKeyMiddleware),
-  FileController.download
-);
-publicRouter.get(
-  "/api/public/translations",
-  asyncHandler(apiKeyMiddleware),
-  TranslationController.getAll
-);
+publicRouter.get("/api/public/projects", ProjectController.getAll);
+publicRouter.get("/api/public/projects/count", ProjectController.count);
+publicRouter.get("/api/public/tools/", ToolController.getAll);
+publicRouter.post("/api/public/messages/", MessageController.create);
+publicRouter.get("/api/public/files/download/:id", FileController.download);
+publicRouter.get("/api/public/translations", TranslationController.getAll);
