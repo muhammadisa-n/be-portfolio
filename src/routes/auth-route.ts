d7e49d232ebba65d4add65ab852740ce9b951ff2
muhammadisa-n <muhammadisa226@gmail.com>
2026-05-13 21:12:49 +0700

    update implement middleware app key for all routes

--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -3,8 +3,13 @@ import { asyncHandler } from "../utils/async-handler";
 import { authMiddleware } from "../middleware/auth-middleware";
 
 import { AuthController } from "../controllers/auth-controller";
+import { apiKeyMiddleware } from "../middleware/api-key-middleware";
 export const authRouter = express.Router();
-authRouter.post("/api/auth/register", AuthController.register);
+authRouter.post(
+  "/api/auth/register",
+  asyncHandler(apiKeyMiddleware),
+  AuthController.register
+);
 authRouter.post("/api/auth/login", AuthController.login);
 authRouter.get("/api/auth/me", asyncHandler(authMiddleware), AuthController.me);
 authRouter.post(
